155b9eb8beee8dc8778b61e9b5f7bfef0a68d806
s33604 <s33604@pjwstk.edu.pl>
2025-03-13 11:06:38 +0100

    zad 1

--- Main.java
+++ Main.java
@@ -1,6 +1,38 @@
 public class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
     }
 
-}
+    public static int findDominant(int[] array) {
+        if (array == null || array.length == 0) {
+            return -1;
+        }
+
+        int candidate = array[0];
+        int count = 1;
+
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] == candidate) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    candidate = array[i];
+                    count = 1;
+                }
+            }
+        }
+
+        count = 0;
+        for (int num : array) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+
+        if (count > array.length / 2) {
+            return candidate;
+        } else {
+            return -1;
+        }
+    }
